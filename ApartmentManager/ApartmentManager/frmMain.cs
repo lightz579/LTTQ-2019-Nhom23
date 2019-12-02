7f43a3a8080dbe7a842b591850921259417ce2d3
lightz579 <52767888+lightz579@users.noreply.github.com>
2019-12-02 16:24:27 +0700

    Update frmMain.cs

--- ApartmentManager/ApartmentManager/frmMain.cs
+++ ApartmentManager/ApartmentManager/frmMain.cs
@@ -16,5 +16,37 @@ namespace ApartmentManager
         {
             InitializeComponent();
         }
+
+        private void btnQuanLyNguoiDan_Click(object sender, EventArgs e)
+        {
+            frmNguoiDan my_frmNguoiDan = new frmNguoiDan();
+            my_frmNguoiDan.ShowDialog();
+        }
+
+        private void btnQuanLyNhanVien_Click(object sender, EventArgs e)
+        {
+            frmNhanVien my_frmNhanVien = new frmNhanVien();
+            my_frmNhanVien.ShowDialog();
+        }
+
+        private void btnQuanLyDichVu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangKyDichVu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTraCuuNguoiDan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTraCuuNhanVien_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
